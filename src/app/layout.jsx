1ccc62df0b3e7990d43afeb401be069ae50946fa
yash chokhani <yashchokhani95@gmail.com>
2023-06-22 23:30:13 +0530

    added fontawesome nextjs changes

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,5 @@
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-config.autoAddCss = false;
 import "./globals.css";
 import { Inter } from "next/font/google";
 import NavBar from "./Navbar";
@@ -49,6 +48,7 @@ export const metadata = {
   description: "yash chokhani portfolio website",
 };
 export default function RootLayout({ children }) {
+  config.autoAddCss = false;
   return (
     <html lang="en" className={`scroll-smooth`}>
       <body className="w-100 dark:bg-gray-900">
