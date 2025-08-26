e70dc2b8766032a5455430e12fe1f38eadcdd83c
Yash Chokhani <yashchokhani95@gmail.com>
2025-08-27 02:36:47 +0530

    congig: s3 domain

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["chokhaniyash-public-bucket.s3.ap-south-1.amazonaws.com","leetcard.jacoblin.cool"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
