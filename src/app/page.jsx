3600f6f38de83f46e3dca248a2d40da52f5fbfa4
Yash Chokhani <yashchokhani95@gmail.com>
2025-08-27 02:37:25 +0530

    coding profile added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,12 +2,14 @@ import About from "./About";
 import Home from "./Home";
 import Contact from "./Contact";
 import Projects from "./Projects";
+import CodingProfiles from "./CodingProfiles";
 function Page() {
   return (
     <div>
       <Home />
       <About />
       <Projects />
+      <CodingProfiles />
       <Contact />
     </div>
   );
